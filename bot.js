39f034f6e9f03b6c4777f8f46fe6ef41ab06bffb
Zhutler <145991870+Zhutler@users.noreply.github.com>
2026-04-22 02:29:04 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,8 @@ const QRCode = require('qrcode');
 const fs = require('fs');
 const path = require('path');
 
-const bot = new Telegraf('8770505563:AAEE8UeScHMw-4zJekTODyVuHUdtGcr0K9Q'); 
+// Бот берет токен из скрытых настроек Railway
+const bot = new Telegraf(process.env.BOT_TOKEN); 
 const ADMIN_IDS = ['789355423', '821782817', '678439277']; // Не забудь айди Хироши
 const APP_URL = 'https://zhutler.github.io/nyako-tickets/app.html?v=5';
 const SCANNER_URL = 'https://zhutler.github.io/nyako-tickets/scanner.html?v=1';
